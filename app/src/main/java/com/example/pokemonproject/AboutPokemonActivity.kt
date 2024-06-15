dbef056c7707bacd4811498223181b12506a9b9f
UdovikArtem <artem.ud2003@example.com>
2024-06-15 17:05:55 +0300

    Make layout and add some pokemons

---
+++ app/src/main/java/com/example/pokemonproject/AboutPokemonActivity.kt
@@ -0,0 +1,41 @@
+package com.example.pokemonproject
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pokemonproject.databinding.ActivityAboutPokemonBinding
+import com.example.pokemonproject.repository.PokemonRepository
+
+class AboutPokemonActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutPokemonBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutPokemonBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pokemonId = intent.getIntExtra("id", -1)
+
+        val pokemon = PokemonRepository.getPokemonById(pokemonId)
+
+        if (pokemon != null) {
+            binding.pokemonImage.setImageResource(pokemon.image)
+            binding.nameEmpty.text = pokemon.name
+            binding.weightEmpty.text = "${pokemon.weight} kg"
+            binding.heightEmpty.text = "${pokemon.height} cm"
+            var types = ""
+            for (type in pokemon.type) {
+                types += type
+                if(type != pokemon.type.last()){
+                    types += ", "
+                }
+            }
+            binding.typeEmpty.text = types
+        }
+
+        binding.backButton.setOnClickListener {
+            this.finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/pokemonproject/repository/PokemonRepository.kt
@@ -0,0 +1,54 @@
+package com.example.pokemonproject.repository
+
+import com.example.pokemonproject.R
+import com.example.pokemonproject.model.Pokemon
+
+object PokemonRepository {
+    private val pokemons: List<Pokemon> = listOf(
+        Pokemon(
+            id = 1,
+            name = "Bulbasaur",
+            weight = 69,
+            height = 7,
+            type = listOf("Grass","Poison"),
+            image = R.drawable.pokemon1
+        ),
+        Pokemon(
+            id = 2,
+            name = "Ivysaur",
+            weight = 130,
+            height = 10,
+            type = listOf("Grass","Poison"),
+            image = R.drawable.pokemon2
+        ),
+        Pokemon(
+            id = 3,
+            name = "Venusaur",
+            weight = 1000,
+            height = 20,
+            type = listOf("Grass","Poison"),
+            image = R.drawable.pokemon3
+        ),
+        Pokemon(
+            id = 4,
+            name = "Charmander",
+            weight = 85,
+            height = 6,
+            type = listOf("Fire"),
+            image = R.drawable.pokemon4
+        ),
+        Pokemon(
+            id = 5,
+            name = "Charmeleon",
+            weight = 190,
+            height = 11,
+            type = listOf("Fire"),
+            image = R.drawable.pokemon5
+        )
+    )
+    fun getPokemonById(id: Int): Pokemon?{
+        return pokemons.find {
+            it.id == id
+        }
+    }
+}
