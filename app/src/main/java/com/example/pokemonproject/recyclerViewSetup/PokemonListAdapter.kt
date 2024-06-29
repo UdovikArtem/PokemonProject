e6730c6c1b2051649fcfbb8d0ed1bd3bbf9dc0f6
UdovikArtem <artem.ud2003@example.com>
2024-06-29 17:31:40 +0300

    use only differ

--- app/src/main/java/com/example/pokemonproject/recyclerViewSetup/PokemonListAdapter.kt
+++ app/src/main/java/com/example/pokemonproject/recyclerViewSetup/PokemonListAdapter.kt
@@ -3,20 +3,18 @@ package com.example.pokemonproject.recyclerViewSetup
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokemonproject.databinding.PokemonCardBinding
 import com.example.pokemonproject.model.Pokemon
 
 class PokemonListAdapter : RecyclerView.Adapter<PokemonViewHolder>() {
 
-    private var items = emptyList<Pokemon>()
-
     private val differ = AsyncListDiffer(this, DiffCallback)
 
     var onClick: (Pokemon) -> Unit = { }
 
     fun submitItems(newItems: List<Pokemon>) {
-        items = newItems
         differ.submitList(newItems)
     }
 
@@ -29,6 +27,6 @@ class PokemonListAdapter : RecyclerView.Adapter<PokemonViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
-        holder.bind(items[position], onClick)
+        holder.bind(differ.currentList[position], onClick)
     }
 }
